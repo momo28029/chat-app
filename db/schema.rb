e381bd1cb2b476c9acbda7f230fe435dcccffc82
momo28029 <momotis915@gmail.com>
2020-12-02 22:04:30 +0900

    ユーザー情報表示の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_053254) do
+ActiveRecord::Schema.define(version: 2020_12_01_023937) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
